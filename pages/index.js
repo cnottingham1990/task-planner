dd4c351388fc9434988d924d03a2ef74b2737a07
cnottingham1990 <nottingham.cody@gmail.com>
2024-12-12 20:29:17 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { TaskPlanner } from '../components/TaskPlanner';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <TaskPlanner />
+    </div>
+  );
+}
